d239e8386e03e21cd1f0ae44460ae1f176624397
johnpc <ioannis.anevlavis.8003@student.uu.se>
2021-02-05 13:36:45 +0200

    TPCC ArgoDSM/Pthreads version is complete

--- ARGO/TPCC/simple_queue.h
+++ ARGO/TPCC/simple_queue.h
@@ -1,72 +1,72 @@
 /*
-  Author: Vaibhav Gogte <vgogte@umich.edu>
-          Aasheesh Kolli <akolli@umich.edu>
+Author: Vaibhav Gogte <vgogte@umich.edu>
+Aasheesh Kolli <akolli@umich.edu>
 */
 
 //#include <iostream>
 #define QUEUE_SIZE 20
 
 class simple_queue {
-  private:
-    long entries[QUEUE_SIZE];
-    long head;
-    long tail;
-  public:
-    simple_queue() {
-      head = 0;
-      tail = 0;
-    }
+	private:
+		long entries[QUEUE_SIZE];
+		long head;
+		long tail;
+	public:
+		simple_queue() {
+			head = 0;
+			tail = 0;
+		}
 
-    ~simple_queue() {}
+		~simple_queue() {}
 
-    bool empty() {
-      return (head == tail);
-    }
+		bool empty() {
+			return (head == tail);
+		}
 
-    bool full() {
-      if(tail == 0)
-        return (head == QUEUE_SIZE-1);
-      return (head == tail-1);
-    }
+		bool full() {
+			if(tail == 0)
+				return (head == QUEUE_SIZE-1);
+			return (head == tail-1);
+		}
 
-    int size() {
-      if(head >= tail) {
-        return head - tail;
-      }
-      else {
-        return (QUEUE_SIZE - tail + head);
-      }
-    }
+		int size() {
+			if(head >= tail) {
+				return head - tail;
+			}
+			else {
+				return (QUEUE_SIZE - tail + head);
+			}
+		}
 
-    bool push(long entry) {
-      if(full())
-        return false;
-      entries[head] = entry;
-      if(head == QUEUE_SIZE-1)
-        head = 0;
-      else
-        head++;
-      return true;
-    }
+		bool push(long entry) {
+			if(full())
+				return false;
+			entries[head] = entry;
+			if(head == QUEUE_SIZE-1)
+				head = 0;
+			else
+				head++;
+			return true;
+		}
 
-    long front() {
-      return entries[tail];
-    }
+		long front() {
+			return entries[tail];
+		}
 
-    bool pop() {
-      if(empty())
-        return false;
-      if(tail == QUEUE_SIZE-1)
-        tail = 0;
-      else
-        tail++;
-      return true;
-    }
+		bool pop() {
+			if(empty())
+				return false;
+			if(tail == QUEUE_SIZE-1)
+				tail = 0;
+			else
+				tail++;
+			return true;
+		}
 
-    //void printQueue() {
-    //  std::cout<<"head tail "<<head<<" "<<tail<<std::endl;
-    //  for(int i=0; i<QUEUE_SIZE; i++) {
-    //    std::cout<<i<<" "<<entries[i]<<std::endl;
-    //  }
-    //}
+		//void printQueue() {
+		//  std::cout<<"head tail "<<head<<" "<<tail<<std::endl;
+		//  for(int i=0; i<QUEUE_SIZE; i++) {
+		//    std::cout<<i<<" "<<entries[i]<<std::endl;
+		//  }
+		//}
 };
--- ARGO/TPCC/tpcc_nvm.cc
+++ ARGO/TPCC/tpcc_nvm.cc
@@ -1,6 +1,6 @@
 /*
- Author: Vaibhav Gogte <vgogte@umich.edu>
-         Aasheesh Kolli <akolli@umich.edu>
+Author: Vaibhav Gogte <vgogte@umich.edu>
+Aasheesh Kolli <akolli@umich.edu>
 
 
 This file models the TPCC benchmark.
@@ -15,84 +15,97 @@ This file models the TPCC benchmark.
 
 #include "tpcc_db.h"
 
-#define NUM_ORDERS 10000000
-#define NUM_THREADS 12 
+#define NUM_ORDERS 10000
+#define NUM_THREADS 4 
 
 #define NUM_WAREHOUSES 1
-#define NUM_ITEMS 10000
+#define NUM_ITEMS 100
 #define NUM_LOCKS NUM_WAREHOUSES*10 + NUM_WAREHOUSES*NUM_ITEMS
+
+int workrank;
+int numtasks;
+
+// Macro for only node0 to do stuff
+#define WEXEC(inst) ({ if (workrank == 0) inst; })
 
 TPCC_DB* tpcc_db;
 
 
 void initialize() {
-  tpcc_db = (TPCC_DB *)malloc(sizeof(TPCC_DB));
-  tpcc_db->initialize(NUM_WAREHOUSES, NUM_THREADS, NUM_LOCKS);
-  
-  fprintf(stderr, "Created tpcc at %p\n", (void *)tpcc_db);
+	tpcc_db = argo::conew_<TPCC_DB>();
+	WEXEC(tpcc_db->initialize(NUM_WAREHOUSES, NUM_THREADS*numtasks, NUM_LOCKS));
+	WEXEC(fprintf(stderr, "Created tpcc at %p\n", (void *)tpcc_db));
 }
 
 
 //void new_orders(TxEngine* tx_engine, int tx_engn_type, TPCC_DB* tpcc_db, int thread_id, int num_orders, int num_threads, int num_strands_per_thread, std::atomic<bool>*wait) {
 void* new_orders(void* arguments) {
 
-  int thread_id = *((int*)arguments);
+	int thread_id = *((int*)arguments);
 
-  for(int i=0; i<NUM_ORDERS/NUM_THREADS; i++) {
-    int w_id = 1;
-    //There can only be 10 districts, this controls the number of locks in tpcc_db, which is why NUM_LOCKS = warehouse*10
-    int d_id = tpcc_db->get_random(thread_id, 1, 10);
-    int c_id = tpcc_db->get_random(thread_id, 1, 3000);
+	for(int i=0; i<NUM_ORDERS/(NUM_THREADS*numtasks); i++) {
+		int w_id = 1;
+		//There can only be 10 districts, this controls the number of locks in tpcc_db, which is why NUM_LOCKS = warehouse*10
+		int d_id = tpcc_db->get_random(thread_id, 1, 10);
+		int c_id = tpcc_db->get_random(thread_id, 1, 3000);
 
-    tpcc_db->new_order_tx(thread_id, w_id, d_id, c_id);
-  }
-  return 0;
+		tpcc_db->new_order_tx(thread_id, w_id, d_id, c_id);
+	}
+	return 0;
 }
 
 int main(int argc, char* argv[]) {
+	argo::init(500*1024*1024UL);
 
-  std::cout<<"in main"<<std::endl;
-  struct timeval tv_start;
-  struct timeval tv_end;
-  std::ofstream fexec;
-  fexec.open("exec.csv",std::ios_base::app);
+	workrank = argo::node_id();
+	numtasks = argo::number_of_nodes();
 
-  initialize();
+	WEXEC(std::cout<<"in main"<<std::endl);
+	struct timeval tv_start;
+	struct timeval tv_end;
+	std::ofstream fexec;
+	WEXEC(fexec.open("exec.csv",std::ios_base::app));
+
+	initialize();
 
 
-  std::cout<<"num_threads, num_orders = "<< NUM_THREADS <<", "<<NUM_ORDERS <<std::endl;
+	WEXEC(std::cout<<"num_threads, num_orders = "<< NUM_THREADS*numtasks <<", "<<NUM_ORDERS <<std::endl);
 
-  std::cout<<"done with initialization"<<std::endl;
+	WEXEC(std::cout<<"done with initialization"<<std::endl);
 
-  tpcc_db->populate_tables();
+	WEXEC(tpcc_db->populate_tables());
 
-  std::cout<<"done with populating tables"<<std::endl;
+	WEXEC(std::cout<<"done with populating tables"<<std::endl);
+	argo::barrier();
 
-  pthread_t threads[NUM_THREADS];
-  int id[NUM_THREADS];
+	pthread_t threads[NUM_THREADS];
+	int global_tid[NUM_THREADS];
 
-  gettimeofday(&tv_start, NULL);
+	gettimeofday(&tv_start, NULL);
 
-  for(int i=0; i<NUM_THREADS; i++) {
-    id[i] = i;
-    pthread_create(&threads[i], NULL, new_orders, (void *)(id+i));
-  }
+	for(int i=0; i<NUM_THREADS; i++) {
+		global_tid[i] = workrank*NUM_THREADS + i;
+		pthread_create(&threads[i], NULL, new_orders, (void *)(global_tid+i));
+	}
 
-  for(int i=0; i<NUM_THREADS; i++) {
-    pthread_join(threads[i], NULL);
-  }
-  gettimeofday(&tv_end, NULL);
-  fprintf(stderr, "time elapsed %ld us\n",
-          tv_end.tv_usec - tv_start.tv_usec +
-              (tv_end.tv_sec - tv_start.tv_sec) * 1000000);
+	for(int i=0; i<NUM_THREADS; i++) {
+		pthread_join(threads[i], NULL);
+	}
+	argo::barrier();
+	gettimeofday(&tv_end, NULL);
+	WEXEC(fprintf(stderr, "time elapsed %ld us\n",
+				tv_end.tv_usec - tv_start.tv_usec +
+				(tv_end.tv_sec - tv_start.tv_sec) * 1000000));
 
-  fexec << "TPCC" << ", " << std::to_string((tv_end.tv_usec - tv_start.tv_usec) + (tv_end.tv_sec - tv_start.tv_sec) * 1000000) << std::endl;
+	WEXEC(fexec << "TPCC" << ", " << std::to_string((tv_end.tv_usec - tv_start.tv_usec) + (tv_end.tv_sec - tv_start.tv_sec) * 1000000) << std::endl);
 
-  fexec.close();
-  free(tpcc_db);
+	WEXEC(fexec.close());
+	argo::codelete_(tpcc_db);
 
-  std::cout<<"done with threads"<<std::endl;
+	WEXEC(std::cout<<"done with threads"<<std::endl);
 
-  return 0;
+	argo::finalize();
+
+	return 0;
 
 }
